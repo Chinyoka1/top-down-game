75fea2dd249be1d0388d21acfd5cb1ff11f5dc37
flynn <flynn.jeschke@gmail.com>
2024-06-06 13:19:51 +0200

    comments

--- Assets/+++Workdata/Scripts/Inventory/InventoryManager.cs
+++ Assets/+++Workdata/Scripts/Inventory/InventoryManager.cs
@@ -81,15 +81,19 @@ public class InventoryManager : MonoBehaviour
             // skip if it has
             if (inventorySlots[i].HasItem()) continue;
             
+            // try to fill the slot if there is an item
             if (a < states.Count)
             {
                 StateInfo stateInfo = gameState.GetStateInfo(states[a].id);
                 stateInfo.amount = states[a].amount;
+                // if the item amount is greater than the stack size, create full slot and reduce amount in every
+                // iteration until there is no rest amount left
                 if (stateInfo.amount > stateInfo.stackSize)
                 {
                     int rest = stateInfo.amount - stateInfo.stackSize;
                     for (int j = 0; rest > 0; j++)
                     {
+                        // create slot with full stack, return rest and assign it as new amount
                         rest = inventorySlots[i+j].SetStateInfoAndReturnRest(stateInfo);
                         stateInfo.amount = rest;
                     }
